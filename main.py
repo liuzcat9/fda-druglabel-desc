ddf5a4738c693c933239f8b3c41f5aec62736528
Catherine L <liucatherine97@gmail.com>
2020-06-03 14:28:47 -0400

    Allow id-based network for top products, with duplicate brand names

--- main.py
+++ main.py
@@ -65,11 +65,11 @@ def rank_purpose(drug1_df):
 # returns: list of similar product names
 def find_similar_products_purpose(drug1_df, original_purpose_list, original):
     valid_df = drug1_df.dropna() # exclude all rows with columns of null
-    valid_df = valid_df.loc[valid_df["brand_name"] != original] # exclude self
+    valid_df = valid_df.loc[valid_df["id"] != original] # exclude self
 
     similar_products = set()
     for purpose in original_purpose_list:
-        similar_products.update(valid_df.loc[valid_df["purpose"].str.contains(purpose), "brand_name"].values.tolist())
+        similar_products.update(valid_df.loc[valid_df["purpose"].str.contains(purpose), "id"].tolist())
 
     return list(similar_products)
 
@@ -78,19 +78,23 @@ def find_similar_products_purpose(drug1_df, original_purpose_list, original):
 def find_matching_indic(drug1_df, similar_products, original):
     nlp = English()
     valid_df = drug1_df.dropna()  # exclude all rows with columns of null
-    valid_df = valid_df.loc[valid_df["brand_name"] != original]  # exclude self
+    valid_df = valid_df.loc[valid_df["id"] != original]  # exclude self
 
     # set original indications
-    original_indic_set = set(clean_list(drug1_df.loc[drug1_df["brand_name"] == original, "indications_and_usage"].values[0], nlp))
+    original_indic_set = set(clean_list(drug1_df.loc[drug1_df["id"] == original, "indications_and_usage"].values[0], nlp))
     # within similar purpose products, find the ones with top similarities in indications and usage
-    top_indic = dict()
+    top_indic = []
 
     for similar_product in similar_products:
         # data frame already wiped of invalid null rows
-        similar_indic_list = set(clean_list(valid_df.loc[valid_df["brand_name"] == similar_product, "indications_and_usage"].values[0], nlp))
-        top_indic[similar_product] = len(original_indic_set.intersection(similar_indic_list))
+        similar_indic_set = set(clean_list(valid_df.loc[valid_df["id"] == similar_product, "indications_and_usage"].values[0], nlp))
 
-    top_indic_df = pd.DataFrame(top_indic.items(), columns=["Product", "Match_Words"])
+        # create columns of ID, product, and matching words
+        top_indic.append([similar_product,
+                         valid_df.loc[valid_df["id"] == similar_product]["brand_name"].values[0],
+                          len(original_indic_set.intersection(similar_indic_set))])
+
+    top_indic_df = pd.DataFrame(top_indic, columns=["ProductID", "ProductName", "Match_Words"])
     top_indic_df = top_indic_df.sort_values(by="Match_Words", ascending=False)
 
     return top_indic_df
@@ -108,59 +112,75 @@ def main():
         # obtain information in a dataframe
         for o in objects:
             drug1_temp.append([o["purpose"][0] if "purpose" in o.keys() else None,
+                          o["id"] if "id" in o.keys() else None,
                           o["openfda"]["brand_name"][0] if ("openfda" in o.keys() and "brand_name" in o["openfda"].keys()) else None,
                           o["indications_and_usage"][0] if "indications_and_usage" in o.keys() else None])
             drug1.append(o)
-    t1 = time.time()
 
+    # add second file and combine
+    drug2_temp = []
+    with open("drug-label-0002-of-0008.json") as file:
+        objects = ijson.items(file, "results.item")
+
+        # obtain information in a dataframe
+        for o in objects:
+            drug2_temp.append([o["purpose"][0] if "purpose" in o.keys() else None,
+                          o["id"] if "id" in o.keys() else None,
+                          o["openfda"]["brand_name"][0] if ("openfda" in o.keys() and "brand_name" in o["openfda"].keys()) else None,
+                          o["indications_and_usage"][0] if "indications_and_usage" in o.keys() else None])
+
+    t1 = time.time()
     print(t1 - t0)
 
     # visualize dataframe of drug info
-    drug1_df = pd.DataFrame(drug1_temp, columns = ["purpose", "brand_name", "indications_and_usage"])
+    drug1_df = pd.DataFrame(drug1_temp, columns = ["purpose", "id", "brand_name", "indications_and_usage"])
+    drug2_df = pd.DataFrame(drug2_temp, columns=["purpose", "id", "brand_name", "indications_and_usage"])
+    drug1_df = pd.concat([drug1_df, drug2_df])
 
     # drug1 is a list of all viable objects
-    # print(drug1_df) # how many drugs there are
-    #
-    # # # use source: https://gist.github.com/deekayen/4148741 to eliminate mundane words
-    # # with open("common_english.txt") as f:
-    # #     mundane = f.read().splitlines() # read without newline character
-    #
-    # # 1: generate key purposes
+    print(drug1_df) # how many drugs there are
+
+    # use source: https://gist.github.com/deekayen/4148741 to eliminate mundane words
+    with open("common_english.txt") as f:
+        mundane = f.read().splitlines() # read without newline character
+
+    # 1: generate key purposes
     # rank_purpose(drug1_df)
-    #
+
     print("Full length: ", str(len(drug1_df)))
 
     # 2: Venn Diagram similarity between most similar drugs based on purpose keyword/indication matches
-    # # choose a drug
-    # original = "H. Pylori Plus"
-    # # find drug purpose
-    # original_purpose = drug1_df.loc[drug1_df["brand_name"] == original, "purpose"].values[0]
-    # # find top purpose keyword
-    # original_purpose_list = clean_list(original_purpose, nlp)
-    #
-    # # find similar products by purpose
-    # similar_products = find_similar_products_purpose(drug1_df, original_purpose_list, original)
-    #
-    # # within similar products, find matching indications for same-purpose medications
-    # top_indic_df = find_matching_indic(drug1_df, similar_products, original)
-    #
-    # original_indic_set = set(
-    #     clean_list(drug1_df.loc[drug1_df["brand_name"] == original, "indications_and_usage"].values[0], nlp))
-    #
-    # print(top_indic_df)
-    #
-    # # plot top 3 by matching indications to original product
-    # plt.figure()
-    # venn = matplotlib_venn.venn3([original_indic_set,
-    #                               set(clean_list(drug1_df.loc[drug1_df["brand_name"] == top_indic_df.iloc[0]['Product'], "indications_and_usage"].values[0], nlp)),
-    #                               set(clean_list(drug1_df.loc[drug1_df["brand_name"] == top_indic_df.iloc[1]['Product'], "indications_and_usage"].values[0], nlp))],
-    #                               set_labels = [original, top_indic_df.iloc[0]['Product'], top_indic_df.iloc[1]['Product']])
-    # plt.title("Indication Word Matches in Common Purpose Product Labels", fontsize=10)
-    # plt.show()
+    # choose a drug
+    original = "b3eebddf-53f5-4f30-a071-ad70152ee97a" # H. Pylori Plus
+    # find drug purpose
+    original_purpose = drug1_df.loc[drug1_df["id"] == original, "purpose"].values[0]
+    # find top purpose keyword
+    original_purpose_list = clean_list(original_purpose, nlp)
+
+    # find similar products by purpose
+    similar_products = find_similar_products_purpose(drug1_df, original_purpose_list, original)
+
+    # within similar products, find matching indications for same-purpose medications
+    top_indic_df = find_matching_indic(drug1_df, similar_products, original)
+
+    original_indic_set = set(
+        clean_list(drug1_df.loc[drug1_df["id"] == original, "indications_and_usage"].values[0], nlp))
+
+    print(top_indic_df)
+
+    # plot top 3 by matching indications to original product
+    plt.figure()
+    original_name = drug1_df.loc[drug1_df["id"] == original]["brand_name"].values[0]
+    venn = matplotlib_venn.venn3([original_indic_set,
+                                  set(clean_list(drug1_df.loc[drug1_df["id"] == top_indic_df.iloc[0]['ProductID'], "indications_and_usage"].values[0], nlp)),
+                                  set(clean_list(drug1_df.loc[drug1_df["id"] == top_indic_df.iloc[1]['ProductID'], "indications_and_usage"].values[0], nlp))],
+                                  set_labels = [original_name, top_indic_df.iloc[0]['ProductName'], top_indic_df.iloc[1]['ProductName']])
+    plt.title("Indication Word Matches in Common Purpose Product Labels", fontsize=10)
+    plt.show()
 
     # 2b: Use purpose to find top products to compare
     # set drug purpose
-    original_purpose = "antiseptic"
+    original_purpose = "sunscreen"
 
     # FUNC: find similar products by purpose as list
     purpose_df = drug1_df.dropna()  # exclude all rows with columns of null
@@ -168,44 +188,47 @@ def main():
     print("Has all non-null: ", str(purpose_df.count(axis=0)))
 
     similar_products = []
-    similar_products.extend(purpose_df.loc[purpose_df["purpose"].str.contains(original_purpose)]["brand_name"].tolist()) # append each one separately
+    similar_products.extend(purpose_df.loc[purpose_df["purpose"].str.contains(original_purpose)]["id"].tolist()) # append each one separately
 
     print("Length of similar products: ", str(len(similar_products)))
 
     # using first product of selected purpose, find matching indications for same-purpose medications
-    product = similar_products[0]
-    top_indic_df = find_matching_indic(drug1_df, similar_products[1:], product)
+    product_id = similar_products[0]
+    top_indic_df = find_matching_indic(drug1_df, similar_products[1:], product_id)
 
     print(top_indic_df)
 
     # 3. Generate a graph node network of top products and their similarity to each other
-    # permute through all combinations of original and top 2 products to numerically obtain the venn diagram similarities
-    product_list = [product]
+    # permute through all combinations of original and top x products to numerically obtain the venn diagram similarities
+    product_list = [product_id]
     product_list.extend(list(top_indic_df.iloc[:, 0]))
     itr_products = product_list
+
     product_comb = list(itertools.combinations(itr_products, 2)) # convert permutations of product names into list
 
     # Generate a network graph from the venn diagram interactions
     venn_G = nx.Graph()
 
     # iterate through products and generate nodes with attributes
-    for product in itr_products:
-        venn_G.add_node(product, name=product,
-                        purpose=drug1_df.loc[drug1_df["brand_name"] == product]["purpose"])
+    for product_id in itr_products:
+        venn_G.add_node(product_id, name=drug1_df.loc[drug1_df["id"] == product_id]["brand_name"],
+                        purpose=drug1_df.loc[drug1_df["id"] == product_id]["purpose"])
 
     # iterate through all combinations and find similarities,
     # generate graph with edges weighted by similarity
     for comb in product_comb:
-        set1 = set(clean_list(drug1_df.loc[drug1_df["brand_name"] == comb[0], "indications_and_usage"].values[0], nlp))
-        set2 = set(clean_list(drug1_df.loc[drug1_df["brand_name"] == comb[1], "indications_and_usage"].values[0], nlp))
+        set1 = set(clean_list(drug1_df.loc[drug1_df["id"] == comb[0], "indications_and_usage"].values[0], nlp))
+        set2 = set(clean_list(drug1_df.loc[drug1_df["id"] == comb[1], "indications_and_usage"].values[0], nlp))
 
         # set weight of edges to reflect closeness of relationships (inverse)
         relation = len(set1.intersection(set2))
         venn_G.add_edge(comb[0], comb[1], weight=relation)
 
+    print("Number of edges: ", str(len(venn_G.edges("cde70a36-38fa-44b4-9036-d6a5b1ea7a48"))))
+
     # Display the network graph from the venn diagram interactions
     plot = figure(title="Network of Top Similar Drugs by Indications and Usage", x_range=(-5, 5), y_range=(-5, 5),
-                  tools="", toolbar_location=None)
+                  tools="pan,lasso_select,box_select", toolbar_location="right")
     # generate hover capabilities
     node_hover_tool = HoverTool(tooltips=[("name", "@name"), ("purpose", "@purpose")], show_arrow = False)
     plot.add_tools(node_hover_tool, BoxZoomTool(), ResetTool())
