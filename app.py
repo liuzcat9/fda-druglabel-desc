7d397b17d5a0d349cf2af5c904fe84932fa701fb
Catherine L <liucatherine97@gmail.com>
2020-07-12 17:56:46 -0400

    Model rudimentary home page

--- app.py
+++ app.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', obs=observe_data.test_observation())
+    purposes = ["Sunscreen", "Analgesic", "Pain Relief"]
+    fields = ["Indications and Usage", "Route"]
+    return render_template('index.html',
+                           purposes=purposes, fields=fields,
+                           obs=observe_data.test_observation())
+
+@app.route('/res', methods=['GET', 'POST'])
+def result():
+    if request.method == 'POST':
+        purpose = request.form["purpose_res"]
+        field = request.form["field_res"]
+
+    return render_template('result.html', purpose=purpose, field=field)
 
 if __name__ == '__main__':
     app.run(port=33507)
