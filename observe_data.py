80c71fb336a840f0e0ee851ec9ca8acc51582d10
Catherine L <liucatherine97@gmail.com>
2020-08-14 19:53:53 -0400

    Add new purpose cluster static images

--- observe_data.py
+++ observe_data.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import parse_json, preprocessing
 
 def plot_product_type(drug_df):
+    print("Full dataset:", str(drug_df.shape))
     # plot OTC/Prescription/NaN for full drug (purpose) dataset
     product_type_counts = drug_df["product_type"].value_counts()
     percentages = [num / len(drug_df.index) * 100.0 for index, num in product_type_counts.items()]
@@ -14,7 +15,7 @@ def plot_product_type(drug_df):
 
     plt.figure()
     plt.pie(percentages, labels=labels, autopct='%1.1f%%')
-    plt.title("Distribution of OTC/Prescription/Unlisted Drugs in Subset")
+    plt.title("Distribution of OTC/Prescription/Unlisted Drugs in Purpose Dataset")
     plt.show()
 
 # plot numbers of drug labels by purpose cluster for tokenized purpose dataset
@@ -24,7 +25,7 @@ def plot_cluster_distribution(purpose_df, purpose_key):
 
     # all purposes
     print(sum(cluster_counts))
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(10, 10))
     plt.xticks(rotation=90)
     plt.bar(labels, cluster_counts)
     plt.xlabel("Labels of Clusters")
@@ -39,7 +40,7 @@ def plot_cluster_distribution(purpose_df, purpose_key):
 
     cluster_counts = trunc_df["purpose_cluster"].value_counts()
 
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(10, 10))
     plt.xticks(rotation=90)
     plt.bar(labels, cluster_counts)
     plt.xlabel("Labels of Clusters")
@@ -52,56 +53,56 @@ def test_observation():
     return list(np.arange(9))
 
 def main():
-    purposes = {"Hand Sanitizer and Cleanser": 'antiseptic bacteria hand skin decrease',
-                "Sterile Sanitizer and Wipes": 'hand rub product dry sterilization',
-                "Hand Sanitizer": 'sanitizer hand soap potentially water',
-                "Miscellaneous (Mainly Prescription) Drugs": 'indicate usage patient symptom tablet',
-                "Antibacterial Sanitizer": 'handwash decrease bacteria skin antibacterial',
-                "Sunscreen and Moisturizer": 'sun skin sunscreen protection aging',
-                "Pain Relief": 'pain relief temporary minor reliever',
-                "Anti-itch and Bug Relief": 'poison minor insect bite itch',
-                "Sunburn Prevention (Sunscreen)": 'sunscreen sunburn prevent help octinoxate',
-                "Acne Treatments": 'acne blemish pimple new blackhead',
-                "Allergy Relief": 'nose itchy runny allergy hay',
-                "Nasal and Cold Relief": 'nasal congestion fever temporarily cough',
-                "First Aid and Sleep Aids": 'aid cut scrape burn infection',
-                "Additional Hand Sanitizers and Wipes": 'sanitizer hand bacteria skin help',
-                "Additional Foaming and Cleansing Sanitizers": 'repeat recommend bacteria decrease skin',
-                "Muscle and Joint Relief": 'muscle sprain analgesic simple backache',
-                "Anti-dandruff and Antifungal": 'dandruff itch tinea flake athlete',
-                "Skin/Acne Treatment": 'acne medication management rosacea vulgaris',
-                "Aspirin and Additional Pain Relief": 'pain ache fever minor temporarily',
-                "Cough Relief": 'cough suppressant throat irritation sore',
-                "Skin Repair and Rash Relief": 'protect skin protectant help chap',
-                "Antiperspirant": 'underarm antiperspirant wetness reduce perspiration',
-                "Homeopathic Remedies": 'homeopathic base traditional review statement',
-                "Acid Reducer and Heartburn Relief": 'stomach heartburn acid indigestion sour',
-                "Anti-diarrheal": 'diarrhea diarrheal traveler anti symptom',
-                "Fever and Cold Reducer": 'fever pain flu throat sore',
-                "Eye and Discomfort Relief": 'eye lubricant irritation discomfort dryness',
-                "Chest Congestion Relief": 'bronchial cough mucus loosen phlegm',
-                "Viral and Bacterial/Fungal Prevention": 'herpe infection vaginal alzheimer acyclovir',
-                "Hypnotics and Bipolar Disorder": 'disorder bipolar 14 clinical study',
-                "Oral Medication": 'oral temporary direction relief headache',
-                "Diabetes Control": 'mellitus diabetes glycemic type diet',
-                "Blood Pressure Control": 'blood pressure cardiovascular reduction risk',
-                "Narcotics, Opioids, Analgesics": 'opioid analgesia tolerate alternative analgesic',
-                "Ulcer Treatment": 'ulcer gerd duodenal week erosive',
-                "Anxiety Relief, Muscle Relaxer": 'anxiety disorder panic tension term',
-                "Toothpaste and Fluoride": 'anticavity cavity aids dental prevention',
-                "Bacterial Infection Treatment": 'infection cause susceptible streptococcus susceptibility',
-                "Nausea and Vomiting Prevention": 'nausea vomiting antiemetic motion emetogenic',
-                "Skin Condition Treatment": 'corticosteroid pruritic dermatosis responsive manifestation',
-                "Laxatives": 'constipation laxative movement bowel generally',
-                "High Cholesterol Treatment": 'ldl diet patient risk hypercholesterolemia',
-                "Anti-depressants": 'depressive major disorder week depression',
-                "Joint and Arthritis Relief": 'sign tablet rheumatoid arthritis osteoarthritis',
-                "Anti-gas": 'gas bloating refer commonly antigas',
-                "Anti-nausea": 'upset stomach diarrhea nausea overindulgence',
-                "Anti-seizure": 'seizure partial epilepsy onset adjunctive',
-                "Hypothyroidism Treatment": 'hypothyroidism thyroid levothyroxine goiter pituitary',
-                "Nicotine": 'smoking quitting nicotine craving withdrawal',
-                "Miscellaneous Anti-inflammatory": 'acute allergic dermatitis disease arthritis'}
+    purposes = {"Skin Antiseptic": 'skin aid antiseptic antibiotic help',
+                "Hand Sanitizer": 'sanitizer hand antiseptic skin wipe',
+                "Antiseptic, Sanitizer, and Wipes (2)": 'antiseptic skin deodorant formulate instant',
+                "Sanitizer Gel (3)": 'bacteria disease cause decrease skin',
+                "Hand Sanitizer and Wipes (4)": 'antibacterial agent wipe hand soap',
+                "Hand Sanitizer and Wipes (5)": 'antimicrobial hand sanitizer purpose topical',
+                "Hand Sanitizer and Handwash (6)": 'fact drug box otc section',
+                "Hand Sanitizer (7)": 'sanitizer skin antiseptic deodorant available',
+                "Alcohol Disinfectant": 'sterilization disinfection rinseing disinfe ction',
+                "Moisturizer and Sunscreen": 'sunscreen protectant skin lightener lip',
+                "Sunscreen and Makeup (2)": 'sun skin protection measure risk',
+                "Sunscreen (3)": 'sunscreen octinoxate octisalate avobenzone octocrylene',
+                "Skin Renewal": 'wrinkle anti brightening skin whiten',
+                "Skin Protection": 'protectant skin ointment sunscreen section',
+                "Wart and Callus Removers": 'remover wart corn callus purpose',
+                "Anti-Itch": 'itch anti antipruritic cream relief',
+                "Acne Control": 'medication acne product antiacne hyland',
+                "Acne Cleanser (2)": 'acne gel pimple cream blemish',
+                "Topical Analgesic": 'topical analgesic anesthetic menthol camphor',
+                "External Analgesic (2)": 'external analgesic protectant skin antibiotic',
+                "Antiseptics and First Aid": 'aid antiseptic antimicrobial topical pain',
+                "Muscle and Allergy Relief": 'relief temporary pain muscle minor',
+                "Allergy Decongestant (2)": 'decongestant nasal antihistamine expectorant suppressant',
+                "Allergy Relief (3)": 'antihistamine succinate doxylamine suppressant guaifenesin',
+                "Allergies and Cramp Relief (4)": 'symptom relieve allergy reliever pain',
+                "Sleep Aid and Pain Relief": 'nighttime sleep aid reliever pain',
+                "Headache Relief": 'headache temporary relief migraine dull',
+                "Pain Relief": 'reliever pain fever reducer aid',
+                "Nighttime Cold Relief": 'fever reducer suppressant reliever decongestant',
+                "Homeopathic Remedies": 'homeopathic food statement review administration',
+                "Cough Decongestant": 'expectorant suppressant cough decongestant nasal',
+                "Acetaminophen Dextromethorphan Cough Suppressant (2)": 'mg hcl acetaminophen phenylephrine dextromethorphan',
+                "Cough Drops": 'oral anesthetic suppressant cough analgesic',
+                "Eye Relief": 'lubricant eye laxative glycol redness',
+                "Anticavity Fluoride": 'anticavity rinse antihypersensitivity antigingivitis antisensitivity',
+                "Toothpaste and Mouthwash": 'toothpaste anticavity antigingivitis antisensitivity whiten',
+                "Anti-Dandruff": 'dandruff anti dermatitis seborrheic psoriasis',
+                "Antiperspirant and Deodorant": 'antiperspirant purpose aluminum chlorohydrate zirconium',
+                "Acid Reducers": 'acid reducer antacid salicylic otc',
+                "Antacids": 'antacid antigas gas anti antidiarrheal',
+                "Anti-Diarrheal": 'diarrheal stomach upset anti antidiarrheal',
+                "Anti-Gas": 'antigas antacid antiflatulent hydroxide simethicone',
+                "Antiseptic Mouth Rinse": 'antiplaque antigingivitis antibacterial 060 092',
+                "Anti-Nausea": 'antiemetic meclizine hydrochloride hcl oxide',
+                "Antifungals": 'antifungal vaginal purpose cream section',
+                "Laxatives": 'stimulant laxative respiratory reliever aromatic',
+                "Natural Laxatives (2)": 'laxative saline phosphate sodium monobasic',
+                "Hemorrhoid Treatment": 'vasoconstrictor protectant local anesthetic 25',
+                "Stool Softeners": 'stool softener laxative stimulant softner',
+                "Nicotine": 'smoking stop aid regain cessation'}
 
     # reverse dictionary
     purpose_key = {val: key for key, val in purposes.items()}
@@ -109,8 +110,8 @@ def main():
     json_list = ["drug-label-0001-of-0009.json", "drug-label-0002-of-0009.json", "drug-label-0003-of-0009.json",
                  "drug-label-0004-of-0009.json", "drug-label-0005-of-0009.json", "drug-label-0006-of-0009.json",
                  "drug-label-0007-of-0009.json", "drug-label-0008-of-0009.json", "drug-label-0009-of-0009.json"]
-    drug_df = parse_json.parse_and_read_drugs(json_list, "indic_full_drug_df")
-    purpose_df = preprocessing.read_preprocessed_to_pkl("purpose_indic_full_drug_df")
+    drug_df = parse_json.parse_and_read_drugs(json_list, "drug_df")
+    purpose_df = preprocessing.read_preprocessed_to_pkl("purpose_drug_df")
 
     plot_product_type(drug_df)
     plot_cluster_distribution(purpose_df, purpose_key)
