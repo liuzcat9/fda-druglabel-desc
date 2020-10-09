cc0698c3152f83ffabef16ed51f68d23419c9206
Catherine L <liucatherine97@gmail.com>
2020-10-08 20:19:46 -0400

    Recolor frontend and organize About sections

--- app.py
+++ app.py
@@ -95,14 +95,14 @@ disabled_field_dict = {'acid reducer antacid salicylic otc': [], 'acne gel pimpl
                        'toothpaste anticavity antigingivitis antisensitivity whiten': [], 'topical analgesic anesthetic menthol camphor': [],
                        'vasoconstrictor protectant local anesthetic 25': [], 'wrinkle anti brightening skin whiten': []}
 
-postgres_engine = create_engine(os.getenv("POSTGRES_URI"))
+# postgres_engine = create_engine(os.getenv("POSTGRES_URI"))
 
 @app.route('/')
 def index():
-    print(postgres_engine.execute("""SELECT brand_name, purpose_cluster
-                                      FROM purpose_drug_labels
-                                      WHERE purpose_cluster = 'aid antiseptic antimicrobial topical pain'
-                                      LIMIT 5;""").fetchall())
+    # print(postgres_engine.execute("""SELECT brand_name, purpose_cluster
+    #                                   FROM purpose_drug_labels
+    #                                   WHERE purpose_cluster = 'aid antiseptic antimicrobial topical pain'
+    #                                   LIMIT 5;""").fetchall())
     reversed_fields = {val: key for key, val in fields.items()}
     return render_template('index.html',
                            purposes=purposes, fields=fields,
